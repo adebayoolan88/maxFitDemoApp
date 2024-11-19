f526704df9a88953c46f3145b574e1f2ffdecdc3
adebayo <adebayoolan88@gmail.com>
2024-11-18 22:29:22 -0600

    Update Goal Page Navigation

--- app/src/main/java/com/example/maxfitdemoapp/MainActivity7.java
+++ app/src/main/java/com/example/maxfitdemoapp/MainActivity7.java
@@ -1,8 +1,9 @@
 package com.example.maxfitdemoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,12 +14,18 @@ public class MainActivity7 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main7);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView updateGoalsSubmit = findViewById(R.id.UpdateGoalsSumbit);
+        updateGoalsSubmit.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity7.this, MainActivity8.class);
+            startActivity(intent);
         });
     }
 }
