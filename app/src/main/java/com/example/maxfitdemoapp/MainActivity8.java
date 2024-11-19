5c2ce30712253f9154a1ecc81cfc4ab2736c460d
adebayo <adebayoolan88@gmail.com>
2024-11-18 22:37:44 -0600

    HomePage Navigtion and Add Workout Page Navigation

--- app/src/main/java/com/example/maxfitdemoapp/MainActivity8.java
+++ app/src/main/java/com/example/maxfitdemoapp/MainActivity8.java
@@ -1,8 +1,9 @@
 package com.example.maxfitdemoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,12 +14,25 @@ public class MainActivity8 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main8);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageView addWorkoutPageNav = findViewById(R.id.addWorkoutPageNav);
+        addWorkoutPageNav.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity8.this, MainActivity9.class);
+            startActivity(intent);
+        });
+
+
+        ImageView homePageNav = findViewById(R.id.HomePageNav);
+        homePageNav.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity8.this, MainActivity4.class);
+            startActivity(intent);
         });
     }
 }
