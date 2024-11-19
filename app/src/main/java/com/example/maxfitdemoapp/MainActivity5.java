539cdfe018e3f64838045ff4c4f79d01f5bfccc1
adebayo <adebayoolan88@gmail.com>
2024-11-18 22:06:09 -0600

    Progress Page Navigation

--- app/src/main/java/com/example/maxfitdemoapp/MainActivity5.java
+++ app/src/main/java/com/example/maxfitdemoapp/MainActivity5.java
@@ -1,8 +1,9 @@
 package com.example.maxfitdemoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,12 +14,18 @@ public class MainActivity5 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main5);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageView recentWorkoutPage = findViewById(R.id.RecentWorkoutPage);
+        recentWorkoutPage.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity5.this, MainActivity6.class);
+            startActivity(intent);
         });
     }
 }
