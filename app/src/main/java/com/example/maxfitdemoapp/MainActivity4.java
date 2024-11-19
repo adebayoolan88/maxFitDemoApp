a367ad453ef1620d1dc2cad31bd20135ed3a3de9
adebayo <adebayoolan88@gmail.com>
2024-11-18 21:20:22 -0600

    Progress Page Navigation

--- app/src/main/java/com/example/maxfitdemoapp/MainActivity4.java
+++ app/src/main/java/com/example/maxfitdemoapp/MainActivity4.java
@@ -1,8 +1,10 @@
 package com.example.maxfitdemoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,12 +15,18 @@ public class MainActivity4 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main4);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageView navigatetoProgressPage = findViewById(R.id.NavigatetoProgressPage);
+        navigatetoProgressPage.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity4.this, MainActivity5.class);
+            startActivity(intent);
         });
     }
 }
